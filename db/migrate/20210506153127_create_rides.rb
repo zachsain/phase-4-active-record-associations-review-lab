7e00b955dffb03a86d2c38c9221f69dda0ae9465
Zachary Sain <zachsain@gmail.com>
2022-08-10 14:18:21 -0500

    commit tree deeebe5ad64416d83bf447527c9daf05d361dbf8

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,6 +1,9 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
+      t.string :name
+      t.integer :passenger_id
+      t.integer :taxi_id 
 
       t.timestamps
     end
